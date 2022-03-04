27fb0006efb2da0d0637071551c5d2341331735d
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2022-03-04 21:15:17 +0100

    House

---
+++ ZombVasion/Assets/Scripts/RoofFade.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofFade : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
