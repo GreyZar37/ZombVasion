e4a801cd272cf836248f17f42e961daeafb9960f
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2022-03-05 11:31:54 +0100

    House and light

--- ZombVasion/Assets/Scripts/RoofFade.cs
+++ ZombVasion/Assets/Scripts/RoofFade.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class RoofFade : MonoBehaviour
 {
+    public GameObject roof;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.tag == "Player")
+        {
         
+                roof.SetActive(false);
+        
+        }
+       
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+           
+                roof.SetActive(true);
+          
+        }
+    }
+   
 }
