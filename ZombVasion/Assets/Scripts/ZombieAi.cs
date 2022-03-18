c5f1d855eb4a16a8bed4f3a88d981ed01d0e9e79
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2022-03-18 18:17:58 +0100

    vision

--- ZombVasion/Assets/Scripts/ZombieAi.cs
+++ ZombVasion/Assets/Scripts/ZombieAi.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieAi : MonoBehaviour
 {
-    /// <Basic Ai mechanics>
+    [Header("Ai mechanics")]
     private Rigidbody2D rb;
 
     public float coolDown;
@@ -16,34 +17,34 @@ public class ZombieAi : MonoBehaviour
     public Pathfinding.AIDestinationSetter pathfinding;
 
 
-    public Transform target;
+   
 
-    /// <Ai Sight>
+    [Header ("Sight")]
     public bool playerSeen;
-    public float sightDistance;
 
-    float timerSight;
-    public float maxTimerSight;
-    public Transform sight;
+    public float radius;
+    [Range(0,360)]
+    public float angle;
 
+    public GameObject target;
 
+    public LayerMask targetMask, obstructionMask;
 
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<GameObject>();
         rb = this.GetComponent<Rigidbody2D>();
-        timerSight = maxTimerSight;
+
+        StartCoroutine(FovRoutine());
     }
 
 
     void Update()
     {
-        vision();
+       
 
 
         coolDownTimer -= Time.deltaTime;
-        timerSight -= Time.deltaTime;      
      
 
         if (playerSeen == true && pathfinding != null)
@@ -60,11 +61,7 @@ public class ZombieAi : MonoBehaviour
 
         }
 
-        if(timerSight <= 0)
-        {
-            playerSeen = false;
-            timerSight = maxTimerSight;
-        }
+       
 
     }
     private void OnTriggerStay2D(Collider2D collision)
@@ -77,25 +74,46 @@ public class ZombieAi : MonoBehaviour
         }
     }
 
-    public void vision()
+    private IEnumerator FovRoutine()
     {
-        
-        RaycastHit2D hitInfo = Physics2D.Raycast(sight.position,new Vector2(target.position.x, target.position.y), sightDistance);
-        print(hitInfo.point);
-        Debug.DrawLine(sight.position, hitInfo.point, Color.red);
-        if (hitInfo.collider != null)
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(delay);
+
+        while (true)
         {
-           
-            if (hitInfo.collider.tag == "Player")
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius,targetMask);
+        if(rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.position, directionToTarget) < angle / 2)
             {
-
-                print("Was Seen");
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if(Physics.Raycast(transform.position, directionToTarget,distanceToTarget, obstructionMask))
+                {
+                    playerSeen = false;
+                }
+                else
+                {
+                    playerSeen = true;
+                }
             }
-
-
+            else
+            {
+                playerSeen = false;
+            }
         }
-
-
+        else if(playerSeen)
+        {
+            playerSeen = false;
         }
-
+    }
 }
